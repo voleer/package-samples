8504a93896178f0df991ec6a3823e398a772d930
Alex Tkatch <alext@bittitan.com>
2019-03-24 00:38:41 +0800

    Cleanup top-level package format

--- first/install.ps1
+++ first/install.ps1
@@ -1,3 +1,11 @@
+<#
+.Description
+Package initialization script.
+
+Contains any initialization and configuration commands that
+should be executed before any of the tasks are launched.
+#>
+
 # Install modules
 Install-Module -Name AzureRM -RequiredVersion 6.13.1
 
--- first/scripts/init.ps1
+++
@@ -1,6 +0,0 @@
-<#
-.Description
-This script is executed after all the modules for the package
-have been installed and loaded but before any of the tasks
-are launched.
-#>

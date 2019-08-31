28409afbed62defb9000e3800e367ab1a669b5a2
Benjamin Briskorn <Bbriskorn@BitTitan.com>
2019-08-30 17:34:24 -0700

    Add get file uris

--- getting-started/tasks/save-and-retrieve-settings/save-and-retrieve-settings.ps1
+++ getting-started/tasks/save-and-retrieve-settings/save-and-retrieve-settings.ps1
@@ -4,46 +4,62 @@ if (Test-Path $localContextPath) {
     . $localContextPath
 }
 
-# Log
-Write-Information "Saving instance settings"
-
 # Call data exchange API
+Write-Information "Saving instance settings"
 $context.SaveText("voleer://workspace.instance/sample-settings.json", "Sample instance settings")
 
-# Read back saved settings back
-$restoredValue = $context.GetText("voleer://workspace.instance/sample-settings.json")
+# Retrieve matching files
+Write-Information "Retrieving matching files"
+$uris = $context.GetFileUris("voleer://workspace.instance/sample-settings");
+Write-Information "Retrieved file name:"
+Write-Information $uris[0]
+
+# Read back saved settings
+$restoredValue = $context.GetText(uris[0])
 Write-Verbose "Restored value:"
 Write-Verbose $restoredValue
 
-# Log
-Write-Information "Saving template settings"
-
 # Call data exchange API
+Write-Information "Saving template settings"
 $context.SaveText("voleer://workspace.template/sample-settings.json", "Sample template settings")
 
-# Read back saved settings back
-$restoredValue = $context.GetText("voleer://workspace.template/sample-settings.json")
+# Retrieve matching files
+Write-Information "Retrieving matching files"
+$uris = $context.GetFileUris("voleer://workspace.template/sample-settings");
+Write-Information "Retrieved file name:"
+Write-Information $uris[0]
+
+# Read back saved settings
+$restoredValue = $context.GetText(uris[0])
 Write-Verbose "Restored value:"
 Write-Verbose $restoredValue
 
-# Log
-Write-Information "Saving package settings"
-
 # Call data exchange API
+Write-Information "Saving package settings"
 $context.SaveText("voleer://workspace.package/sample-settings.json", "Sample package settings")
 
-# Read back saved settings back
-$restoredValue = $context.GetText("voleer://workspace.package/sample-settings.json")
+# Retrieve matching files
+Write-Information "Retrieving matching files"
+$uris = $context.GetFileUris("voleer://workspace.package/sample-settings");
+Write-Information "Retrieved file name:"
+Write-Information $uris[0]
+
+# Read back saved settings
+$restoredValue = $context.GetText($uris[0])
 Write-Verbose "Restored value:"
 Write-Verbose $restoredValue
 
-# Log
-Write-Information "Saving vendor settings"
-
 # Call data exchange API
+Write-Information "Saving vendor settings"
 $context.SaveText("voleer://workspace.vendor/sample-settings.json", "Sample vendor settings")
 
-# Read back saved settings back
-$restoredValue = $context.GetText("voleer://workspace.vendor/sample-settings.json")
+# Retrieve matching files
+Write-Information "Retrieving matching files"
+$uris = $context.GetFileUris("voleer://workspace.vendor/sample-settings");
+Write-Information "Retrieved file name:"
+Write-Information $uris[0]
+
+# Read back saved settings
+$restoredValue = $context.GetText($uris[0])
 Write-Verbose "Restored value:"
 Write-Verbose $restoredValue
